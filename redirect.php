31d55d5fdffda0dc007b90e315d58c609a0fff76
Gowtham Alaguraj <gowtham25alaguraj@outlook.com>
2019-02-08 11:45:23 +0530

    Added files for Google Drive upload

---
+++ redirect.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+	<title>OAuth redirect</title>
+	<!-- HTTP 1.1 -->
+	<meta http-equiv="Cache-Control" content="no-store"/>
+	<!-- HTTP 1.0 -->
+	<meta http-equiv="Pragma" content="no-cache"/>
+	<!-- Prevents caching at the Proxy Server -->
+	<meta http-equiv="Expires" content="0"/>
+	
+	<script src="js/cookies.min.1.2.3.js"></script>
+	<script type="text/javascript">		
+		function getQueryStringValue(url, key) {
+			return decodeURIComponent(url.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1")); 
+		}
+		
+		var currentPageUrl=window.location.href;
+		var isAuthCodeAvailable = (currentPageUrl.indexOf("code=") != -1);
+		if(isAuthCodeAvailable){
+			var authCode = getQueryStringValue(currentPageUrl, "code");
+			if(Cookies.enabled){
+				//expire cookie automatically in 1 minute
+				Cookies.set('authCode', authCode, { expires: 60 });
+				Cookies.set('authRedirectUri', currentPageUrl, { expires: 60 }); 
+			}
+		}
+	</script>
+  </head>
+  <body>
+     This is page will close automatically in 5 seconds.
+  </body>
+</html>
